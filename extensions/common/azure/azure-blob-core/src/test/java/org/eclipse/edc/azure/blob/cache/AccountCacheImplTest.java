b420f6fd0a698fbfdc0259b538134dd9012c6284
bmg13 <gabcmonteiro@gmail.com>
2024-05-07 13:58:13 +0100

    Added Unit Tests to Account Cache.

--- extensions/common/azure/azure-blob-core/src/test/java/org/eclipse/edc/azure/blob/cache/AccountCacheImplTest.java
+++ extensions/common/azure/azure-blob-core/src/test/java/org/eclipse/edc/azure/blob/cache/AccountCacheImplTest.java
@@ -26,6 +26,7 @@ class AccountCacheImplTest {
 
     private static final String ENDPOINT_TEMPLATE = "https://%s.blob.core.windows.net";
     private static final String ACCOUNT_NAME = "test_account";
+    private static final String OTHER_ACCOUNT_NAME = "other_test_account";
     private static final String ACCOUNT_KEY = "test_key";
 
     private final AccountCache accountCache = new AccountCacheImpl(mock(Vault.class), ENDPOINT_TEMPLATE);
@@ -42,5 +43,32 @@ class AccountCacheImplTest {
         var result = accountCache.getBlobServiceClient(ACCOUNT_NAME);
         assertThat(result.getAccountName()).isEqualTo(ACCOUNT_NAME);
         assertEquals(result.getAccountUrl(), createEndpoint(ENDPOINT_TEMPLATE, ACCOUNT_NAME));
+    }
+
+    @Test
+    void getAccount_sameAccount_succeeds() {
+        var firstAccount = accountCache.getBlobServiceClient(ACCOUNT_NAME);
+        var secondAccount = accountCache.getBlobServiceClient(ACCOUNT_NAME);
+        assertThat(firstAccount).isEqualTo(secondAccount);
+    }
+
+    @Test
+    void getAccount_differentAccounts_succeeds() {
+        var firstAccount = accountCache.getBlobServiceClient(ACCOUNT_NAME);
+        var secondAccount = accountCache.getBlobServiceClient(OTHER_ACCOUNT_NAME);
+        assertThat(firstAccount).isNotEqualTo(secondAccount);
+    }
+
+    @Test
+    void getAccount_sameAccountWithKey_succeeds() {
+        var firstAccount = accountCache.getBlobServiceClient(ACCOUNT_NAME, ACCOUNT_KEY);
+        var secondAccount = accountCache.getBlobServiceClient(ACCOUNT_NAME, ACCOUNT_KEY);
+        assertThat(firstAccount).isEqualTo(secondAccount);
+    }
+
+    @Test
+    void getAccount_WithNullKey_succeeds() {
+        var result = accountCache.getBlobServiceClient(ACCOUNT_NAME, null);
+        assertThat(result.getAccountName()).isEqualTo(ACCOUNT_NAME);
     }
 }
